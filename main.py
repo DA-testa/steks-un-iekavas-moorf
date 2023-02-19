05b256e7e7d3ae9022c8799ed8c0315a56ee1ae2
Bodya <hocaleangu@gmail.com>
2023-02-19 11:49:37 +0200

    done

--- main.py
+++ main.py
@@ -13,18 +13,33 @@ def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
-            # Process opening bracket, write your code here
-            pass
-
+           opening_brackets_stack.append(Bracket(next,i+1))
+           pass
         if next in ")]}":
-            # Process closing bracket, write your code here
+            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
+                return i+1
+            opening_brackets_stack.pop()
             pass
 
 
 def main():
-    text = input()
-    mismatch = find_mismatch(text)
-    # Printing answer, write your code here
+    select_input = input("input - F or I")
+    if select_input.upper() == "F":
+        file_path = input("choose file (input path)")
+        with open(file_path, "r") as f:
+            text = f.read()
+            mismatch = find_mismatch(text)
+            if mismatch == None:
+                print("Success")
+            else:
+                print(mismatch)
+    elif select_input.upper() == "I":
+        text = input()
+        mismatch = find_mismatch(text)
+        if mismatch == None:
+            print("Success")
+        else:
+            print(mismatch)
 
 
 if __name__ == "__main__":
